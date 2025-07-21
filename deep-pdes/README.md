5b77284f2a87d909df3e7c2285854727e4f935e3
Zheng Ma <mazhengcn@outlook.com>
2025-07-21 17:15:03 +0800

    feat: add new styles and configurations for deep learning presentations
    - Introduced a new CSS file for styling the presentation with custom variables and transitions.
    - Created a new UnoCSS configuration to include web fonts and shortcuts for text gradients.
    - Added a Vite configuration file to extend the base configuration.
    - Removed outdated README and component files related to previous presentations.
    - Deleted unused images and slides to clean up the project structure.

--- deep-pdes/README.md
+++
@@ -1,3 +0,0 @@
-# Solve Kinetic Equations with Deep Learning
-
-2025/07/20 - [CSIAM-BDAI 2025](http://bdai2025.icrp.xjtu.edu.cn)
--- deep-pdes/src/components/Emphasis.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <span rounded-lg px2 py1 class="text-[#c99efb] bg-[#161616ee]">
-
-    <slot />
-
-  </span>
-</template>
